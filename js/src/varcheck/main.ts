7f2f72068c43062a5fd7f74baf8c8861fa2824dd
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2024-10-12 09:42:23 -0400

    async

--- js/src/varcheck/main.ts
+++ js/src/varcheck/main.ts
@@ -1,4 +1,6 @@
 import * as fs from 'fs';
+import { promisify } from 'util';
+import { exec } from 'child_process';
 
 import { parse } from '@ein/bash-parser';
 
@@ -35,16 +37,22 @@ function isCommandAssignment(c: any): boolean {
     return c.prefix != null && c.prefix.length == 1 && c.prefix[0].type == 'AssignmentWord';
 }
 
-function checkCommand(c: any, script: Script, vars: string[]) {
+const checkCommand = async (c: any, script: Script, vars: string[], env: Env) => {
     // console.log(c);
     if (isCommandNsScriptInvocation(c)) {
-        // todo: parse script and deal with promise
+        var childScriptName = c.suffix[2].text;
+        const e = promisify(exec);
+        const { stdout, stderr } = await e('ns_which "' + childScriptName + '"');
+        var path: string = stdout.replace("\n", "");
+        var pass = await varcheck(path, env);
+        /*if (!pass) {
+            console.log('child script failed', env.scripts[1].errors);
+        }*/
     } else if (isCommandAssignment(c)) {
         vars.push(c.prefix[0].text.split("=")[0]);
     } else if (isCommandImport(c)) {
         //var prompt = env.scriptNo == 1;
         for (var i=2; i<c.suffix.length; i++) {
-            console.log("test", c.suffix[i].text, vars);
             if (vars.indexOf(c.suffix[i].text)<0) {
                 script.errors.push({
                     type: "VariableNotSetError",
@@ -54,40 +62,41 @@ function checkCommand(c: any, script: Script, vars: string[]) {
             //console.log("var:", c.suffix[i].text);
         }
     }
+    return true;
 }
 
-function checkScript(name: string, s: any, env: Env) {
+async function checkScript(name: string, s: any, env: Env) {
     //env.scriptNo += 1;
     var script = {name: name, errors: []};
     env.scripts.push(script);
     for (var i=0; i<s.commands.length; i++) {
         if (s.commands[i].type == 'Command') {
-            checkCommand(s.commands[i], script, env.memory.vars);
+            await checkCommand(s.commands[i], script, env.memory.vars, env);
         }
     }
+    return true;
 }
 
-function run(scriptName: string, ast: any, env: Env) {
+async function run(scriptName: string, ast: any, env: Env) {
     //console.log(ast);
     //console.log('--------------------------------------------');
     //console.log(ast.type);
     if (ast.type == "Script") {
-        checkScript(scriptName, ast, env);
+        await checkScript(scriptName, ast, env);
     }
 }
 
-function varcheck(path: any, env: Env, cb: any) {
+const varcheck = async (path: any, env: Env): Promise<boolean> => {
     var bashContent = fs.readFileSync(path, {encoding: 'utf8'});
     //console.log(bashContent);
     //parse(bashContent);
-    parse(bashContent, {mode: 'bash'}).then(ast => {
-        run(path, ast, env);
-        cb(env);
-    });
+    var ast = await parse(bashContent, {mode: 'bash'});
+    await run(path, ast, env);
+    return env.scripts.filter(s => s.errors.length > 0).length == 0;
 }
 
 
-(function() {
+(async () => {
     var vars: string[] = [];
     var scriptPath: string = "";
     process.argv.forEach(function(val, index, array) {
@@ -103,11 +112,11 @@ function varcheck(path: any, env: Env, cb: any) {
         }
     });
     var env: Env = { memory: { vars: vars }, scripts: [] };
-    var failed: boolean = false;
-    varcheck(scriptPath, env, () => {
+
+    var pass = await varcheck(scriptPath, env);
+    if (!pass) {
         for (var i=0; i<env.scripts.length; i++) {
             for (var j=0; j<env.scripts[i].errors.length; j++) {
-                failed = true;
                 var first = i == 0 && j == 0;
                 var last = i == env.scripts.length - 1 && j == env.scripts[i].errors.length - 1;
                 var error = env.scripts[i].errors[j];
@@ -115,10 +124,8 @@ function varcheck(path: any, env: Env, cb: any) {
                 console.error(error.message+(last?"":"\n"));
             }
         }
-        if (failed) {
-            process.exitCode = 1;
-        }
-    });
+        process.exitCode = 1;
+    }
 })();
 
 
