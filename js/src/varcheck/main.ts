f5565b742c7d793539a4475fd1d4d0ec832ae765
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2024-10-12 07:43:12 -0400

    started on variable checks

--- js/src/varcheck/main.ts
+++ js/src/varcheck/main.ts
@@ -1,27 +1,124 @@
 import * as fs from 'fs';
 
-//import { parse } from 'bash-parser';
-// var parse: any = require('bash-parser');
 import { parse } from '@ein/bash-parser';
 
-//console.log(parse);
 global.structuredClone = (val) => JSON.parse(JSON.stringify(val));
 
-//parse('echo cia').then(o => console.log(o));
+interface Memory {
+    vars: string[];
+}
 
-function varcheck(path: any) {
+interface ScriptError {
+    type: string;
+    message: string;
+}
+
+interface Script {
+    name: string;
+    errors: ScriptError[];
+}
+
+interface Env {
+    memory: Memory;
+    scripts: Script[];
+}
+
+function isCommandNsScriptInvocation(c: any): boolean {
+    return c?.name?.text == '.' && c?.suffix?.length >= 3 && c.suffix[0].text == 'ns' && c.suffix[1].text == 'run' && c.suffix[2].text.length > 0;
+}
+
+function isCommandImport(c: any): boolean {
+    return c?.name?.text != null && c?.suffix != null && c.name.text == '.' && c.suffix.length > 2 && c.suffix[0].text == 'ns' && c.suffix[1].text == 'import';
+}
+
+function isCommandAssignment(c: any): boolean {
+    return c.prefix != null && c.prefix.length == 1 && c.prefix[0].type == 'AssignmentWord';
+}
+
+function checkCommand(c: any, script: Script, vars: string[]) {
+    // console.log(c);
+    if (isCommandNsScriptInvocation(c)) {
+        // todo: parse script and deal with promise
+    } else if (isCommandAssignment(c)) {
+        vars.push(c.prefix[0].text.split("=")[0]);
+    } else if (isCommandImport(c)) {
+        //var prompt = env.scriptNo == 1;
+        for (var i=2; i<c.suffix.length; i++) {
+            console.log("test", c.suffix[i].text, vars);
+            if (vars.indexOf(c.suffix[i].text)<0) {
+                script.errors.push({
+                    type: "VariableNotSetError",
+                    message: `Script: ${script.name}, imports variable: '${c.suffix[i].text}' which is not defined.`
+                });
+            }
+            //console.log("var:", c.suffix[i].text);
+        }
+    }
+}
+
+function checkScript(name: string, s: any, env: Env) {
+    //env.scriptNo += 1;
+    var script = {name: name, errors: []};
+    env.scripts.push(script);
+    for (var i=0; i<s.commands.length; i++) {
+        if (s.commands[i].type == 'Command') {
+            checkCommand(s.commands[i], script, env.memory.vars);
+        }
+    }
+}
+
+function run(scriptName: string, ast: any, env: Env) {
+    //console.log(ast);
+    //console.log('--------------------------------------------');
+    //console.log(ast.type);
+    if (ast.type == "Script") {
+        checkScript(scriptName, ast, env);
+    }
+}
+
+function varcheck(path: any, env: Env, cb: any) {
     var bashContent = fs.readFileSync(path, {encoding: 'utf8'});
-    console.log(bashContent);
+    //console.log(bashContent);
     //parse(bashContent);
-    parse(bashContent, {mode: 'bash'}).then(ast => console.log(ast));
+    parse(bashContent, {mode: 'bash'}).then(ast => {
+        run(path, ast, env);
+        cb(env);
+    });
 }
 
 
-process.argv.forEach(function(val, index, array) {
-    if (index == array.length-1) {
-        console.log(val);
-        varcheck(val);
-    }
-});
+(function() {
+    var vars: string[] = [];
+    var scriptPath: string = "";
+    process.argv.forEach(function(val, index, array) {
+        if (index == array.length-1) {
+            var inVars = val.split(",");
+            for (var i=0; i<inVars.length; i++) {
+                if (inVars[i].length > 0) {
+                    vars.push(inVars[i]);
+                }
+            }
+        } else if (index == array.length-2) {
+            scriptPath = val;
+        }
+    });
+    var env: Env = { memory: { vars: vars }, scripts: [] };
+    var failed: boolean = false;
+    varcheck(scriptPath, env, () => {
+        for (var i=0; i<env.scripts.length; i++) {
+            for (var j=0; j<env.scripts[i].errors.length; j++) {
+                failed = true;
+                var first = i == 0 && j == 0;
+                var last = i == env.scripts.length - 1 && j == env.scripts[i].errors.length - 1;
+                var error = env.scripts[i].errors[j];
+                console.error((first?"":"\n")+error.type);
+                console.error(error.message+(last?"":"\n"));
+            }
+        }
+        if (failed) {
+            process.exitCode = 1;
+        }
+    });
+})();
 
 
